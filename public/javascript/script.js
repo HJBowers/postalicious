cfaa6c0a5884045ce31dc6250edafe92b924b680
Breyana Scales <breyana@Breyanas-MacBook-Pro.local>
2017-03-22 17:26:57 -0700

    added response population backbone

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,3 +1,5 @@
+let request
+
 const populateRequest = () => {
   const formElements = document.mainform.elements
   let elementValuesObject = {}
@@ -54,10 +56,71 @@ Host: ${elementValuesObject.host}
 
   let requestQueryEncoded = encodeURI(requestQuery)
 
-  let request = requestMethodHost + requestHeader + "\n" + requestQueryEncoded + requestBody
+  request = requestMethodHost + requestHeader + "\n" + requestQueryEncoded + requestBody
 
   //WIP
   let requestHTMLified = "<b>" + requestMethodHost + "</b><br>" + requestHeader + "<br><br>" + requestQueryEncoded + requestBody
 
   document.getElementById("request-container").innerHTML = requestHTMLified
+}
+
+const populateResponse = () => {
+  const formElements = document.mainform.elements
+  let elementValuesObject = {}
+
+  for (let element of formElements){
+    elementValuesObject[element.name] = element.value
+  }
+
+  console.log(elementValuesObject)
+
+  const myHeaders = new Headers()
+  if (elementValuesObject.header_key1) {
+    myHeaders.append(elementValuesObject.header_key1, elementValuesObject.header_value1)
+  }
+  if (elementValuesObject.header_key2){
+    myHeaders.append(elementValuesObject.header_key2, elementValuesObject.header_value2)
+  }
+  if (elementValuesObject.header_key3) {
+    myHeaders.append(elementValuesObject.header_key3, elementValuesObject.header_value3)
+  }
+
+  const myInit = {
+    method: elementValuesObject.method,
+    headers: myHeaders,
+  }
+
+  let requestQuery = ''
+
+  if (elementValuesObject.key1) {
+    requestQuery +=
+      encodeURIComponent(elementValuesObject.key1) +
+      "="+ encodeURIComponent(elementValuesObject.value1)
+  }
+  if (elementValuesObject.key2) {
+    requestQuery += "&" +
+      encodeURIComponent(elementValuesObject.key2) +
+      "="+ encodeURIComponent(elementValuesObject.value2)
+  }
+  if (elementValuesObject.key3) {
+    requestQuery += "&" +
+      encodeURIComponent(elementValuesObject.key3) +
+      "="+ encodeURIComponent(elementValuesObject.value3)
+  }
+
+  console.log(requestQuery)
+
+  let url = elementValuesObject.host + "?" + requestQuery
+
+  const myRequest = new Request(url, myInit)
+
+  console.log(myRequest)
+
+  fetch(myRequest)
+    .then(function(response) {
+      console.log(response)
+      let container = document.getElementById("response-container")
+      container.innerText = response.status
+    })
+
 }
